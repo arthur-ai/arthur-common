8d7c1441b86976fba547f421dfcbb6e0581ebaca
Ian McGraw <mcgrawia@umich.edu>
2025-08-25 16:28:55 -0400

    update test to check both schema and schemaless read paths

--- tests/unit/tools/test_duck_db_operations.py
+++ tests/unit/tools/test_duck_db_operations.py
@@ -434,7 +434,33 @@ def test_simple_load_data_with_schema_special_character_names(
     assert res[0][0] == 2
 
 
-def test_simple_load_data_with_large_json_object():
+@pytest.mark.parametrize(
+    "schema",
+    [
+        DatasetSchema(
+            alias_mask={},
+            columns=[
+                DatasetColumn(
+                    id=uuid4(),
+                    source_name="id",
+                    definition=DatasetScalarType(id=uuid4(), dtype=DType.UUID),
+                ),
+                DatasetColumn(
+                    id=uuid4(),
+                    source_name="timestamp",
+                    definition=DatasetScalarType(id=uuid4(), dtype=DType.TIMESTAMP),
+                ),
+                DatasetColumn(
+                    id=uuid4(),
+                    source_name="long_field",
+                    definition=DatasetScalarType(id=uuid4(), dtype=DType.STRING),
+                ),
+            ],
+        ),
+        None,
+    ],
+)
+def test_simple_load_data_with_large_json_object(schema):
     data = [
         {
             "id": "991ff637-2c13-4289-ba17-a23c1c2b20b8",
@@ -443,12 +469,12 @@ def test_simple_load_data_with_large_json_object():
         },
     ]
 
-    conn = DuckDBOperator.load_data_to_duckdb(data)
+    conn = DuckDBOperator.load_data_to_duckdb(data, schema=schema)
     results = conn.sql("SELECT * FROM inferences").fetchall()
     assert len(results) == 1
 
-    # Assert values took on the type defined in the schema, (uuid, datetime) and not the raw types (str, str)
-    for row in results:
-        assert type(row[0]) == UUID
-        assert type(row[1]) == str
-        assert type(row[2]) == str
+    if schema is not None:
+        for row in results:
+            assert type(row[0]) == UUID
+            assert type(row[1]) == datetime
+            assert type(row[2]) == str
