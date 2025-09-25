2ea1783400c4ca154f18ca5f6d5f7259b680a9f6
Tal Erez <tal@arthur.ai>
2025-09-25 15:21:09 -0400

    remove unnecessary test cases after deleting validations

--- tests/unit/models/test_metrics.py
+++ tests/unit/models/test_metrics.py
@@ -20,12 +20,3 @@ def test_base_aggregation_parameter_schema_parameter_key_allowed_characters():
     assert schema_2.parameter_key == "test_parameter_key"
     assert schema_2.friendly_name == "friendly name"
     assert schema_2.description == "Test description"
-
-
-def test_base_aggregation_parameter_schema_parameter_key_allowed_characters_invalid():
-    with pytest.raises(ValueError):
-        BaseAggregationParameterSchema(
-            parameter_key="test_parameter_key-123",
-            friendly_name="Test Parameter Key",
-            description="Test description",
-        )
