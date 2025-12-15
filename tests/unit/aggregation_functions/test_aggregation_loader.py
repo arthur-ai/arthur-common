2dfff4e2e1c5a2750d6375ae8b937359610dc322
Ian McGraw <mcgrawia@umich.edu>
2025-12-15 10:00:43 -0500

    Add tool count aggregation (#73)

--- tests/unit/aggregation_functions/test_aggregation_loader.py
+++ tests/unit/aggregation_functions/test_aggregation_loader.py
@@ -3,7 +3,7 @@ from arthur_common.tools.aggregation_loader import AggregationLoader
 
 def test_aggregation_loader():
     functions = AggregationLoader.load_aggregations()
-    total_expected = 39
+    total_expected = 40
     for func in functions:
         print(func)
     assert len(functions) == total_expected
