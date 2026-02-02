3137a502df9e7a4480ec94637b1dc6e7ba5e27ea
Ibrahim Abouhashish <ibrahimabouhashish@gmail.com>
2026-02-02 09:34:53 -0500

    Add Databricks connector constants and authentication methods

--- src/arthur_common/models/enums.py
+++ src/arthur_common/models/enums.py
@@ -53,6 +53,11 @@ class SnowflakeConnectorAuthenticatorMethods(BaseEnum):
     SNOWFLAKE_PASSWORD = "snowflake_password"
 
 
+class DatabricksConnectorAuthenticatorMethods(BaseEnum):
+    DATABRICKS_PAT = "databricks_pat"
+    DATABRICKS_OAUTH_TOKEN_PASSTHROUGH = "databricks_oauth_token_passthrough"
+
+
 # Using version from arthur-engine, which has str and enum type inheritance.
 # Note: These string values are not arbitrary and map to Presidio entity types: https://microsoft.github.io/presidio/supported_entities/
 class PIIEntityTypes(BaseEnum):
